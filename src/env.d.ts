e99ba2382db8ae9e2573d4cf8a89a392ff5c0361
Dysaca22 <dilanc@uninorte.edu.co>
2024-11-14 21:00:32 -0500

    [ADD] projects cards

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface Project {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+    tag: string;
+}
+
+declare module '@src/js/projects.js' {
+    export const projects: Project[];
+}
---
+++ src/js/projects.js
@@ -0,0 +1,16 @@
+export const projects = [
+    {
+        title: 'Project 1',
+        description: 'Description of Project 1',
+        image: 'https://picsum.photos/1200/600',
+        link: 'https://picsum.photos/1200/600',
+        tag: 'tag1',
+    },
+    {
+        title: 'Project 2',
+        description: 'Description of Project 2',
+        image: 'https://picsum.photos/1200/600',
+        link: 'https://picsum.photos/1200/600',
+        tag: 'tag2',
+    },
+];
